8083273722a2726fbe7e8ca89de298b3ca02ce72
Nishu Rai <54761473+nishu91020@users.noreply.github.com>
2022-03-17 13:11:22 +0530

    Buttons run,reset and back to home set

--- src/Components/Main/Main.js
+++ src/Components/Main/Main.js
@@ -2,6 +2,7 @@ import React from 'react';
 import CodeEditor from '../CodeEditor/CodeEditor';
 import Terminal from '../Terminal/Terminal';
 import './Main.scss';
+import { Button } from '@mui/material';
 import KeyboardArrowRightIcon from '@mui/icons-material/KeyboardArrowRight';
 import CircularProgress from '@mui/material/CircularProgress';
 import axios from '../../axios';
@@ -12,13 +13,12 @@ import themeAction from '../../Actions/theme';
    symbol for run button. Main class provides different features for our webiste, for example error handling for our user input in codeEditor class, likewise many other. */
 
 class Main extends React.Component {
-
-    constructor(props) {
+    constructor (props) {
         super(props);
-        
+
         this.collapseAndShow = this.collapseAndShow.bind(this);
         this.main = React.createRef();
-        
+
         this.state = {
             collapsed: false,
             codeSubmitting: false,
@@ -26,7 +26,7 @@ class Main extends React.Component {
             input: '',
             output: '',
             error: ''
-        }
+        };
 
         this.submitCode = this.submitCode.bind(this);
         this.changeCode = this.changeCode.bind(this);
@@ -34,21 +34,20 @@ class Main extends React.Component {
         this.getCode = this.getCode.bind(this);
     }
 
-    changeCode(code) {
+    changeCode (code) {
         this.setState({
             code: code
-        })
+        });
     }
 
-    changeInput(event) {
+    changeInput (event) {
         this.setState({
             input: event.target.value
-        })
+        });
     }
 
-    async submitCode() {
-        
-        this.setState({codeSubmitting: true});
+    async submitCode () {
+        this.setState({ codeSubmitting: true });
 
         const response = (await axios.post('/api/v1/ide/getcode', {
             language: this.props.selectedLanguage,
@@ -56,63 +55,69 @@ class Main extends React.Component {
             input: this.state.input
         })).data;
 
-        let error_msg= "";
+        let error_msg = '';
 
-        if(response.status_msg === "Time Limit Exceeded") {
+        if (response.status_msg === 'Time Limit Exceeded') {
             error_msg = response.status_msg;
         }
         else {
             error_msg = response.full_compile_error;
         }
 
-        this.setState({codeSubmitting: false, output: response.code_output.join('\n'), error: error_msg});
-
+        this.setState({ codeSubmitting: false, output: response.code_output.join('\n'), error: error_msg });
     }
 
-    collapseAndShow() {
+    collapseAndShow () {
         this.main.current.classList.toggle('collapsed');
-        this.setState(prevState => ({collapsed: !prevState.collapsed}));
+        this.setState(prevState => ({ collapsed: !prevState.collapsed }));
     }
 
-    async getCode() {
-        const response = (await axios.get(`/api/v1/ide/${this.props.selectedId}`)).data
+    async getCode () {
+        const response = (await axios.get(`/api/v1/ide/${this.props.selectedId}`)).data;
         // console.log(response)
-        this.changeCode(response)
+        this.changeCode(response);
     }
 
-    componentDidMount() {
+    componentDidMount () {
         this.getCode();
     }
 
-    componentDidUpdate(prevProps) {
+    componentDidUpdate (prevProps) {
         if (prevProps.selectedId !== this.props.selectedId) {
             this.getCode();
         }
     }
-    
-    render() {
+
+    render () {
         return (
-            <div className = "main" ref = {this.main}>
+            <div className="main" ref={this.main}>
                 <CodeEditor code={this.state.code} changeCode={this.changeCode} />
-                <Terminal output={this.state.output} input = {this.state.input} error= {this.state.error} changeInput = {this.changeInput} isCollapsed = {this.state.collapsed} />
-                <KeyboardArrowRightIcon className = {`showCode ${this.state.collapsed && 'showCode--collapsed'}`} onClick = {this.collapseAndShow} />
-                
-                { this.state.codeSubmitting ? 
-                    <CircularProgress className = "submitCode submitCode--progress" />:
-                    <KeyboardArrowRightIcon className = "submitCode" onClick = {this.submitCode} />
-                }
-                
+                <Terminal output={this.state.output} input={this.state.input} error={this.state.error} changeInput={this.changeInput} isCollapsed={this.state.collapsed} />
+                <KeyboardArrowRightIcon className={`showCode ${this.state.collapsed && 'showCode--collapsed'}`} onClick={this.collapseAndShow} />
+
+                {this.state.codeSubmitting ? (
+                    <Button className="submitCode" variant="contained" color="success">
+                        <CircularProgress size={'1rem'} />
+                    </Button>
+                ) : (
+                    <Button className="submitCode" variant="contained" color="primary" onClick={this.submitCode}>
+                        Run
+                    </Button>
+                )}
+                <Button className="reset " variant="outlined" color="primary">
+                    Reset
+                </Button>
             </div>
         );
-  }
-}
-
-function mapStateToProps(state, ownProps) {
-	return {
-        theme: state.theme,
-        selectedLanguage: state.currentLanguage.selectedLanguage,
-        selectedId: state.currentLanguage.selectedId
     }
 }
 
-export default connect(mapStateToProps, {themeAction})(Main);
+function mapStateToProps (state, ownProps) {
+    return {
+        theme: state.theme,
+        selectedLanguage: state.currentLanguage.selectedLanguage,
+        selectedId: state.currentLanguage.selectedId
+    };
+}
+
+export default connect(mapStateToProps, { themeAction })(Main);
--- src/pages/NotFound/index.js
+++ src/pages/NotFound/index.js
@@ -1,40 +1,40 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { Button } from '@mui/material';
 import './NotFound.scss';
 
-function NotFound() {
-  return (
-    <div className="center">
-      <div className="error">
-        <div className="number">4</div>
-        <div className="illustration">
-          <div className="circle"></div>
-          <div className="clip">
-            <div className="paper">
-              <div className="face">
-                <div className="eyes">
-                  <div className="eye eye-left"></div>
-                  <div className="eye eye-right"></div>
+function NotFound () {
+    return (
+        <div className="center">
+            <div className="error">
+                <div className="number">4</div>
+                <div className="illustration">
+                    <div className="circle" />
+                    <div className="clip">
+                        <div className="paper">
+                            <div className="face">
+                                <div className="eyes">
+                                    <div className="eye eye-left" />
+                                    <div className="eye eye-right" />
+                                </div>
+                                <div className="rosyCheeks rosyCheeks-left" />
+                                <div className="rosyCheeks rosyCheeks-right" />
+                                <div className="mouth" />
+                            </div>
+                        </div>
+                    </div>
                 </div>
-                <div className="rosyCheeks rosyCheeks-left"></div>
-                <div className="rosyCheeks rosyCheeks-right"></div>
-                <div className="mouth"></div>
-              </div>
+                <div className="number">4</div>
             </div>
-          </div>
-        </div>
-        <div className="number">4</div>
-      </div>
 
-      <div className="text">
-        Oops. The page you're looking for doesn't exist. You can click the
-        button below to go back to the homepage
-      </div>
-      <Link className="button" to="/">
-        Back Home
-      </Link>
-    </div>
-  );
+            <div className="text">Oops. The page you're looking for doesn't exist. You can click the button below to go back to the homepage</div>
+            <Button variant="contained" color="primary">
+                <Link to="/" style={{ textDecoration: 'none', color: '#fff' }}>
+                    Back Home
+                </Link>
+            </Button>
+        </div>
+    );
 }
 
 export default NotFound;
